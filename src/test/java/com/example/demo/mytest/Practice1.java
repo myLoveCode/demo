f99474fd41dd6b3bd75b34614c489bf85d73c77a
myLoveCode <1165030656@qq.com>
2017-12-04 01:12:43 +0800

    1:  select ... into ... 2:  delete ... where ... 3:  batch insert ...

--- src/test/java/com/example/demo/mytest/Practice1.java
+++ src/test/java/com/example/demo/mytest/Practice1.java
@@ -1,10 +1,8 @@
 package com.example.demo.mytest;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 class Product {
